b2af1d65a899fa838a1ee62659b539fdef662cbc
axvss <andrevssouza@gmail.com>
2019-05-26 07:19:13 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
+
+
+import sable.lexer.Lexer;
+import sable.lexer.LexerException;
+import sable.node.Token;
+
+
+public class Main {
+	 public static void main(String[] args) throws LexerException, IOException { 
+		Lexer lexer = null;
+		try {
+			lexer = new Lexer (new PushbackReader( 
+			        new FileReader(args[0]), 1024));
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+		}
+		String parada = "EOF" ;
+		Token t = lexer.next() ;
+		String check = t.getClass().getSimpleName() ;
+		while (!check.equals(parada)){
+			if(check.equals("TBlank")) {
+				System.out.println("") ;
+			} else if(check.equals("TSpc")){
+				System.out.print(" ");
+			}else {
+				System.out.print((t.getClass().getSimpleName())) ;
+			}
+			t = lexer.next() ;
+			check = t.getClass().getSimpleName().toString() ;
+		}
+	 }
+}
